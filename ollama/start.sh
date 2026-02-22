9da854e693af98b0b4600d4022d762b52858c5ca
JoanM2K4 <jjsotom2k4@gmail.com>
2026-02-21 21:31:56 -0500

    Fix: Solucion al error de persistencia de Ollama
    Renombramos OLLAMA_MODELS a OLLAMA_MODEL_LIST para evitar conflicto con la variable interna OLLAMA_MODELS de la imagen oficial de Ollama.
    
    OLLAMA_MODELS es usada por Ollama como directorio de almacenamiento de modelos, no como lista de modelos a descargar.
    Al usarla como lista, Ollama interpretaba el valor como path y guardaba los modelos en una ruta incorrecta.
    
    El nuevo nombre elimina el conflicto y asegura que los modelos se guarden correctamente en el volumen montado.

--- ollama/start.sh
+++ ollama/start.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 set -e
 
-if [ -z "$OLLAMA_MODELS" ]; then
-  echo " ERROR: La variable OLLAMA_MODELS no está definida."
+if [ -z "$OLLAMA_MODELS_LIST" ]; then
+  echo " ERROR: La variable OLLAMA_MODELS_LIST no está definida."
   echo " Debes definirla en el archivo .env o en docker-compose.yml"
   exit 1
 fi
@@ -12,11 +12,15 @@ ollama serve &
 
 sleep 5
 
-echo "Modelos a descargar: $OLLAMA_MODELS"
+echo "Modelos a descargar: $OLLAMA_MODELS_LIST"
 
-for model in $OLLAMA_MODELS; do
-  echo "Descargando $model ..."
-  ollama pull "$model"
+for model in $OLLAMA_MODELS_LIST; do
+  if ollama list | awk '{print $1}' | grep -qx "$model"; then
+    echo "$model ya está descargado. Saltando pull."
+  else
+    echo "Descargando $model ..."
+    ollama pull "$model"
+  fi
 done
 
 wait
